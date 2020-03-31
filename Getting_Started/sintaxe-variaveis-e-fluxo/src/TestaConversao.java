5b29723f35c863effa834d20fcded4fce3bbf3ff
Joamir Silveira <48892574+JoamirS@users.noreply.github.com>
2020-03-31 11:25:30 -0300

    Understanding the casting

--- Getting_Started/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
+++ Getting_Started/sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -18,8 +18,6 @@ public class TestaConversao {
 		double valorTotal = valor1 + valor2;
         	/* Como será constatado na instrução print abaixo, double não é a melhor maneira de
         	armazenar valores monetários */
-		
 		System.out.println(valorTotal);
-		
 	}
 }
