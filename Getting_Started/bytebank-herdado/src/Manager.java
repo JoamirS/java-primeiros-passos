b02f5abb49f0ff53a746e7c47bff0a239d8f3183
JoamirS <jmrsilveira@outlook.com>
2020-05-26 11:03:39 -0300

    Update Manager

--- Getting_Started/bytebank-herdado/src/Manager.java
+++ Getting_Started/bytebank-herdado/src/Manager.java
@@ -24,3 +24,9 @@ public class Manager extends Employee {
 	}
 
 }
+
+/*
+ * Uma classe pode ter várias filhas, mas apenas uma mãe.
+ * A partir de uma instancia de uma classe filha, podemos chamar qualquer método público que tenha,
+ * 	sido declarado na classe mãe. 
+ */
