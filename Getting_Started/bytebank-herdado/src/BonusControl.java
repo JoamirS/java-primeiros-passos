06a9bd4a338d6ef7dc38623c77f1dcb116017e32
JoamirS <jmrsilveira@outlook.com>
2020-05-28 11:17:23 -0300

    Control of all employees

---
+++ Getting_Started/bytebank-herdado/src/BonusControl.java
@@ -0,0 +1,14 @@
+
+public class BonusControl {
+
+	private double sumBonus;
+
+	public void register(Employee employeeBonus) {
+		double bonus = employeeBonus.getBonus();
+		this.sumBonus = this.sumBonus + bonus;
+	}
+
+	public double getSum() {
+		return sumBonus;
+	}
+}
