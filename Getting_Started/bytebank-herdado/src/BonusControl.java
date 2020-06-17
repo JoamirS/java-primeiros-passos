e2562cab2d483992305fb3b5ff7831497a1c9ed6
JoamirS <jmrsilveira@outlook.com>
2020-06-16 23:24:45 -0300

    Control Bonus

--- Getting_Started/bytebank-herdado/src/BonusControl.java
+++ Getting_Started/bytebank-herdado/src/BonusControl.java
@@ -2,12 +2,12 @@
 public class BonusControl {
 
 	private double sumBonus;
-
+	
 	public void register(Employee employeeBonus) {
-		double bonus = employeeBonus.getBonus();
-		this.sumBonus = this.sumBonus + bonus;
-	}
-
+        double bonus = employeeBonus.getBonus();
+        this.sumBonus = this.sumBonus + bonus;
+    }
+	
 	public double getSum() {
 		return sumBonus;
 	}
